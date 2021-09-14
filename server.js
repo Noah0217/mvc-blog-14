b645622441a56fbe22a108d2cdfb73d927e67c14
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-09-14 13:52:38 -0600

    declared all dependencies

--- server.js
+++ server.js
@@ -1,1 +1,39 @@
 //dependencies
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const notes = require('./db/db.json'); 
+
+//path
+const mainPath = path.join(__dirname, './public') 
+const db = path.join(__dirname, "./db") 
+
+//server dependencies & port to live server
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//middleware paths for api https://stackoverflow.com/questions/11321635/nodejs-express-what-is-app-use
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.json());
+
+//route to html notes
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(mainPath, 'notes.html'));
+});
+app.get('/api/notes', function(req, res) {
+    res.sendFile(path.join(db, 'db.json'))
+    return res.body
+});
+app.post('/api/notes', function(req, res) {
+    console.log("post route hit")
+    res.sendFile(path.join(db, 'db.json'))
+    return res.body
+});
+app.get("*", function(req, res) {
+    res.sendFile(path.join(mainPath, 'index.html'));
+});
+
+//successfully connected to port
+app.listen(PORT, () => 
+console.log(`successfully connected to http://localhost:${PORT}`)); //https://stackoverflow.com/questions/37929173/significance-of-port-3000-in-express-apps
