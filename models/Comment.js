32c1b377eafc3be3e785401c4f4764b03bc28e8b
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-09-17 22:14:32 -0600

    completed comment and index models

--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,48 @@
-//
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+              model: 'post',
+              key: 'id'
+        },
+    },
+        user_id: {
+            type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    },
+       comment_text: {
+        type: DataTypes.STRING,
+        validate: {
+
+            len: [3]
+        }
+
+       }, {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+
+});
+
+    module.exports = Comment;
+
