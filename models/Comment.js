c5a584a9e711ad2ef99ee2074fc51eb7aadd8e4a
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-09-17 22:33:46 -0600

    cleaned up code

--- models/Comment.js
+++ models/Comment.js
@@ -4,45 +4,51 @@ const sequelize = require('../config/connection');
 class Comment extends Model {}
 
 Comment.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true
-        },
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+      },
 
-        post_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-              model: 'post',
-              key: 'id'
-        },
-    },
-        user_id: {
-            type: DataTypes.INTEGER,
+//user id
+      user_id: {
+        type: DataTypes.INTEGER,
         allowNull: false,
         references: {
-            model: 'user',
-            key: 'id'
+          model: 'user',
+          key: 'id'
         }
-    },
-       comment_text: {
+      },
+
+//post id
+      post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'post',
+          key: 'id'
+        }
+      },
+
+//comment text
+      comment_text: {
         type: DataTypes.STRING,
+        allowNull: false,
         validate: {
-
-            len: [3]
+            len: [2]
         }
+      }
+  },
 
-       }, {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'comment'
-    }
 
-});
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+  }
+);
 
-    module.exports = Comment;
-
+module.exports = Comment;
