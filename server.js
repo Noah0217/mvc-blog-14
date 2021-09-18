01e3ea2e0cd643837c01213854587d0a1fa5478b
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-09-18 00:35:01 -0600

    routes dependency path fixed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //dependencies
 const express = require('express');
-const routes = require('./config/connection');
+const routes = require('./controllers');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
