5adb427e48997fe8e82c8b80695b98b73ada00fe
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-09-17 22:04:01 -0600

    User model complete no errors so far

--- models/User.js
+++ models/User.js
@@ -1,1 +1,61 @@
-//
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+class User extends Model {
+ 
+checkPassword(loginPW) {
+        return bcrypt.compareSync(loginPW, this.password);
+    }
+}
+
+//column tables
+User.init(
+  {
+//id column
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+//username column
+    username: {
+        type: DataTypes.STRING,
+        allowNull:false,
+        unique: true
+    },
+    
+//password column 
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+// password has to be at least 4 characters whatever is in length (len) array changes password character limit
+            len: [4]
+        }
+    }
+  },
+
+//executes password function
+  {
+    hooks: {
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+            
+        },
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
