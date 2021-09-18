307bbdb6910313b8be723077821c6cc456bc0e75
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-09-17 23:28:14 -0600

    dashboard routes complete

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,1 +1,90 @@
-//
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'created_at'
+            ],
+
+            //attributes for both comment and user
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+
+        }).then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('dashboard', { posts, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id',
+                'title',
+                'content',
+                'created_at'
+            ],
+
+            //user attributes
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+
+            //comment attributes    
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+
+        //error message    
+        }).then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'Please try again no post found with this ID.' });
+                return;
+            }
+
+            const post = dbPostData.get({ plain: true });
+            res.render('edit-post', { post, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+router.get('/new', (req, res) => {
+    res.render('new-post');
+});
+
+module.exports = router;
