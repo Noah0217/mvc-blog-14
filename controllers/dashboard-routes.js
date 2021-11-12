0ac14aac853e18c1c942017b3404690df3b75dfc
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-11-12 11:19:42 -0700

    cleaned up attributes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -4,11 +4,14 @@ const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 
+
 router.get('/', withAuth, (req, res) => {
+
     Post.findAll({
             where: {
                 user_id: req.session.user_id
             },
+
             attributes: [
                 'id',
                 'title',
@@ -16,7 +19,6 @@ router.get('/', withAuth, (req, res) => {
                 'created_at'
             ],
 
-            //attributes for both comment and user
             include: [{
                     model: Comment,
                     attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
@@ -30,8 +32,8 @@ router.get('/', withAuth, (req, res) => {
                     attributes: ['username']
                 }
             ]
-
-        }).then(dbPostData => {
+        })
+        .then(dbPostData => {
             const posts = dbPostData.map(post => post.get({ plain: true }));
             res.render('dashboard', { posts, loggedIn: true });
         })
@@ -40,24 +42,24 @@ router.get('/', withAuth, (req, res) => {
             res.status(500).json(err);
         });
 });
+
+
 router.get('/edit/:id', withAuth, (req, res) => {
     Post.findOne({
             where: {
                 id: req.params.id
             },
+            
             attributes: ['id',
                 'title',
                 'content',
                 'created_at'
             ],
-
-            //user attributes
+            
             include: [{
                     model: User,
                     attributes: ['username']
                 },
-
-            //comment attributes    
                 {
                     model: Comment,
                     attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
@@ -67,11 +69,10 @@ router.get('/edit/:id', withAuth, (req, res) => {
                     }
                 }
             ]
-
-        //error message    
-        }).then(dbPostData => {
+        })
+        .then(dbPostData => {
             if (!dbPostData) {
-                res.status(404).json({ message: 'Please try again no post found with this ID.' });
+                res.status(404).json({ message: 'No post found with this id' });
                 return;
             }
 
@@ -86,5 +87,7 @@ router.get('/edit/:id', withAuth, (req, res) => {
 router.get('/new', (req, res) => {
     res.render('new-post');
 });
+
+
 
 module.exports = router;
