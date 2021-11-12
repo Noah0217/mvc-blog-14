09ba390cc0992830dd340773e91940f90dd2eab9
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-11-12 11:41:01 -0700

    seed data changed & fixed

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,9 @@
 const seedUsers = require('./user-seeds');
 const seedPosts = require('./post-seeds');
 const seedComments = require('./comment-seeds');
-
 const sequelize = require('../config/connection');
 
+//all seeds
 const seedAll = async() => {
     await sequelize.sync({ force: true });
     await seedUsers();
